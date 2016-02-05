ce9fa1112e0ecdb665f6671630a795ebc5e5775f
coreyholmes <coreyholmes20@gmail.com>
2016-02-05 03:14:58 +0000

    first commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'pry'
+require 'colorize'
+# require './page'
+
+class Players
+
+def initialize
+  @name = name
+  @health = 3
+  @score = 0
+end
+
+
+
+def computer
+num1 = rand(1..20)
+num2 = rand(1..20)
+p num1
+p num2
+end
+
+computer
+
+
+
+
+
+
+
+generate_question * prompt_player_for_answer * verify_answer
